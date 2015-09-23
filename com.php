014bb15f5bd8e21365c7011fe69b29a8f53f6026
evaristo261093 <lcmaxidesing@gmail.com>
2015-09-23 11:24:19 -0500

    primer paso

---
+++ com.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<H1></H1>
+<FORM ACTION="compara.php" METHOD="GET">
+Introduzca el primer numero:
+<INPUT TYPE="text" NAME="numero1"><BR>
+
+<br>
+Introduzca el segundo numero:
+
+<INPUT TYPE="text" NAME="numero2"><BR>
+
+
+<INPUT TYPE="submit" VALUE="Compara">
+<INPUT TYPE="reset" VALUE="Limpia">
+<INPUT TYPE="submit" VALUE="regresa">
+</FORM>
+</body>
+</html> 
---
+++ fac.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<H1></H1>
+<FORM method= "POST" action="ff.php">
+
+saca el factorial de cualquier numero:
+
+
+<INPUT TYPE="text" name="factor" size=15>
+<br>
+
+<INPUT TYPE="submit"  name="cal "VALUE="Calcular">
+
+</FORM>
+</body>
+</html> 
